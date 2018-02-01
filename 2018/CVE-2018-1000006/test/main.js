83974d1a89cdcf066ab987735a559d69bf057d0c
Beep3r <beep3rl@gmail.com>
2018-02-01 21:24:25 +0800

    Add files via upload

---
+++ 2018/CVE-2018-1000006/test/main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+const dialog = require('electron').dialog
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  win.on('closed', function(){
+    win = null
+  })
+}
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', function(){
+  if (win === null) {
+    createWindow()
+  }
+})
+app.setAsDefaultProtocolClient('chybeta')
